941ce64892b0ffd85b8eb2dc6fd2375cbd6c3af0
Samuel Ferrer <ferrer@csb.bmc.uu.se>
2017-07-29 22:43:53 +0200

    new structure - work and symbolic links

--- superscreening/setup.sh
+++ superscreening/setup.sh
@@ -1,3 +1,11 @@
-mkdir log
-mkdir task
-mkdir report
+sh create_project_structure.sh
+
+# non empty folders
+ln -s work/data data
+ln -s work/template template
+
+# empty folders
+ln -s work/hot hot
+ln -s work/log log
+ln -s work/task task
+ln -s work/report report
